8cc2dde804b0c86167c56bdce303040ba0d4105b
r3sh4p3 <reshape.kek@gmail.com>
2025-03-03 20:39:47 +0100

    Lesson 7

--- main.py
+++ main.py
@@ -1,39 +1,34 @@
-from graphics import Window, Line, Point
-from cell import Cell
+from graphics import Window
+from maze import Maze
 
 
 def main():
     win = Window(800, 600)
 
-    cell_size = 100
-    cells = []
+    num_rows = 3
+    num_cols = 3
+    cell_size_x = 100
+    cell_size_y = 100
+    x1 = 50
+    y1 = 50
 
-    for x in range(3):
-        for y in range(3):
-            x1 = 50 + x * cell_size
-            y1 = 50 + y * cell_size
-            x2 = x1 + cell_size
-            y2 = y1 + cell_size
+    maze = Maze(x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
-            cell = Cell(x1, y1, x2, y2, win)
+    maze.cells[1][0].has_bottom_wall = False
 
-            if x == 1 and y == 0:  # Middle top cell
-                cell.has_bottom_wall = False
-            if x == 1 and y == 1:  # Center cell
-                cell.has_top_wall = False
-                cell.has_left_wall = False
-            if x == 0 and y == 1:  # Left middle cell
-                cell.has_right_wall = False
-            if x == 2 and y == 1:  # Right middle cell
-                cell.has_left_wall = False
-                cell.has_bottom_wall = False
-            if x == 1 and y == 2:  # Bottom middle cell
-                cell.has_top_wall = False
+    maze.cells[1][1].has_top_wall = False
+    maze.cells[1][1].has_left_wall = False
 
-            cells.append(cell)
+    maze.cells[0][1].has_right_wall = False
 
-    for cell in cells:
-        cell.draw()
+    maze.cells[2][1].has_left_wall = False
+    maze.cells[2][1].has_bottom_wall = False
+
+    maze.cells[1][2].has_top_wall = False
+
+    for i in range(num_cols):
+        for j in range(num_rows):
+            maze.cells[i][j].draw()
 
     win.wait_for_close()
 
